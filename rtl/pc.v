814739697e6a22fc0a7b792490b464b457c8f4ca
UgoOdibo04 <uodibo@charlotte.edu>
2025-11-21 00:39:14 -0500

    Add PC register module with clock and reset

--- rtl/pc.v
+++ rtl/pc.v
@@ -1,1 +1,16 @@
+module pc_reg (
+    input        clock,
+    input         reset,
+    input   [31:0] next_pc,
+    output reg  [31:0] pc
+);
+
+  always @(posedge clock or posedge reset) begin
+        if (reset)
+            pc <= 32'h00000000;   
+        else
+            pc <= next_pc;
+    end
+
+endmodule
 
