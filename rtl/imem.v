269199d581da8f73eda6eb9f9cfa12a205494f0b
UgoOdibo04 <uodibo@charlotte.edu>
2025-11-21 00:27:48 -0500

    Create imem module for instruction memory
    Added instruction memory module with initialization.

--- rtl/imem.v
+++ rtl/imem.v
@@ -1,1 +1,14 @@
+module imeme (
+  input [31:0] address,
+  output [31:0] instruction
+);
+  // 2KB 32 Bits Wide
+  reg [31:0] mem [0:511];
 
+ initial begin
+        $readmemh("imem.hex", mem);
+    end
+
+assign instruction = mem[address[31:2]];
+
+endmodule 
