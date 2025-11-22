774745602b752c8a22c7928afdaea86c03c93b2b
UgoOdibo04 <uodibo@charlotte.edu>
2025-11-22 01:45:02 -0500

    Update immediate_generator.v

--- rtl/immediate_generator.v
+++ rtl/immediate_generator.v
@@ -8,28 +8,21 @@ module immediate_generator(
     always @(*) begin
         case (opcode)
 
-            // -----------------------------
-            // I-type (ADDI, LW, JALR, etc.)
-            // imm[31:0] = sign-extend(instr[31:20])
-            // -----------------------------
+          
             7'b0000011,   // LOAD
             7'b0010011,   // I-type ALU (ADDI, ANDI, etc.)
             7'b1100111:   // JALR
                 immediate = {{20{instruction[31]}}, instruction[31:20]};
 
-            // -----------------------------
+       
             // S-type (stores)
-            // imm[31:0] = sign-extend({instr[31:25], instr[11:7]})
-            // -----------------------------
             7'b0100011:
                 immediate = {{20{instruction[31]}},
                               instruction[31:25],
                               instruction[11:7]};
 
-            // -----------------------------
+
             // B-type (branches)
-            // imm = { instr[31], instr[7], instr[30:25], instr[11:8], 0 }
-            // -----------------------------
             7'b1100011:
                 immediate = {{19{instruction[31]}},
                               instruction[31],
@@ -38,18 +31,14 @@ module immediate_generator(
                               instruction[11:8],
                               1'b0};
 
-            // -----------------------------
+    
             // U-type (LUI, AUIPC)
-            // imm = instr[31:12] << 12
-            // -----------------------------
             7'b0110111,  // LUI
             7'b0010111:  // AUIPC
                 immediate = {instruction[31:12], 12'b0};
 
-            // -----------------------------
+           
             // J-type (JAL)
-            // imm = { instr[31], instr[19:12], instr[20], instr[30:21], 0 }
-            // -----------------------------
             7'b1101111:
                 immediate = {{11{instruction[31]}},
                               instruction[31],
