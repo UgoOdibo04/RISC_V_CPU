8fb25a7141646ccc722986251695238508eed6ad
UgoOdibo04 <uodibo@charlotte.edu>
2025-11-22 01:27:07 -0500

    Update alusrc_mux.v

--- rtl/alusrc_mux.v
+++ rtl/alusrc_mux.v
@@ -1,10 +1,12 @@
-module alusrc_mux(
-  input [31:0] register2, immediate
-  input control_signal
-  output [31:0] mux_out
-
+module alusrc_mux (
+    input  wire [31:0] register2,
+    input  wire [31:0] immediate,
+    input  wire        control_signal,
+    output wire [31:0] mux_out
 );
-  assign mux_out = (control_signal == 1'b1) ? register2 : immediate;
-  
+
+    // ALUSrc = 1 → use immediate
+    // ALUSrc = 0 → use rs2_value
+    assign mux_out = (control_signal == 1'b1) ? immediate : register2;
 
 endmodule
