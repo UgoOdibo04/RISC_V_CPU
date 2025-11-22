0c3d61665df9a5b2aeeff6d53ef132e09dc38f60
UgoOdibo04 <uodibo@charlotte.edu>
2025-11-22 01:25:43 -0500

    Refactor control unit outputs to use reg type

--- rtl/control_unit.v
+++ rtl/control_unit.v
@@ -1,25 +1,41 @@
 module control_unit (
-  input [6:0] opcode,
-  output branch,
-  output memread, 
-  output memtoreg, 
-  output memwrite,
-  output alusrc,
-  output regwrite
+    input  wire [6:0] opcode,
+
+    output reg        branch,
+    output reg        memread,
+    output reg        memtoreg,
+    output reg        memwrite,
+    output reg        alusrc,
+    output reg        regwrite
 );
 
-  always @(*) begin
-    case(opcode)
+    always @(*) begin
+        // Default NOP
+        {alusrc, memtoreg, regwrite, memread, memwrite, branch} = 6'b000000;
 
-      7'b0110011 : {alusrc, memtoreg, regwrite, memread, memwrite, branch} <= 6'b001000;
-      7'b0000011 : {alusrc, memtoreg, regwrite, memread, memwrite, branch} <= 6'b111100;
-      7'b0100011 : {alusrc, memtoreg, regwrite, memread, memwrite, branch} <= 6'b100010;
-      7'b0010011 : {alusrc, memtoreg, regwrite, memread, memwrite, branch} <= 6'b101000;
-      7'b1100011 : {alusrc, memtoreg, regwrite, memread, memwrite, branch} <= 6'b101000;
+        case (opcode)
 
-    endcase
-  end 
+            // R-type (ADD, SUB, OR, AND, etc.)
+            7'b0110011 : 
+                {alusrc, memtoreg, regwrite, memread, memwrite, branch} = 6'b001000;
 
+            // LOAD (LB, LH, LW, LBU, LHU)
+            7'b0000011 : 
+                {alusrc, memtoreg, regwrite, memread, memwrite, branch} = 6'b111100;
 
+            // STORE (SB, SH, SW)
+            7'b0100011 : 
+                {alusrc, memtoreg, regwrite, memread, memwrite, branch} = 6'b100010;
 
-endmodule 
+            // I-type ALU (ADDI, XORI, ANDI...)
+            7'b0010011 :
+                {alusrc, memtoreg, regwrite, memread, memwrite, branch} = 6'b101000;
+
+            // BRANCH (BEQ, BNE, etc.)
+            7'b1100011 :
+                {alusrc, memtoreg, regwrite, memread, memwrite, branch} = 6'b100001;
+
+        endcase
+    end 
+
+endmodule
