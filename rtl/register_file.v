ae4bd8dd1475ec36fba3904d5ed420bea930ec37
UgoOdibo04 <uodibo@charlotte.edu>
2025-11-22 00:33:09 -0500

    Fix reset logic and enable register writes
    Modify register file to handle reset and write operations correctly.

--- rtl/register_file.v
+++ rtl/register_file.v
@@ -16,22 +16,21 @@ module register_file(
   input reset
 );
 
-   reg [31:0] reg_memory [31:0]
+  reg [31:0] reg_memory [31:0];
   integer i; 
   //Fill all Registers with 0
-   always @(posedge clock) begin
+  always @(posedge clock or posedge reset) begin
         if (reset) begin
             for (i = 0; i < 32; i = i + 1)
                 reg_memory[i] <= 32'b0;
-        end else begin
-            // synchronous write, but x0 MUST remain 0
-            if (regwrite && (write_reg != 5'd0)) begin
-                reg_memory[write_reg] <= write_data;
-            end
         end
+  end 
+    else if(regwrite) begin
+      reg_memory[write_reg] <= write_data;
     end
-  
-  assign reg1_value = (read_reg1 == 5'd0) ? 32'b0 : reg_memory[read_reg1];
-  assign reg2_value = (read_reg2 == 5'd0) ? 32'b0 : reg_memory[read_reg2];
+  end 
+   
+  assign reg1_value = reg_memory[read_reg1];
+  assign reg2_value = reg_memory[read_reg2];
 
 endmodule 
