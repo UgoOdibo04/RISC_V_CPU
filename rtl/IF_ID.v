6e3ce9d521a87d255fe955dcc8a8fa7d68208d4b
UgoOdibo04 <uodibo@charlotte.edu>
2025-11-22 23:03:21 -0500

    Update IF_ID.v

--- rtl/IF_ID.v
+++ rtl/IF_ID.v
@@ -1,6 +1,7 @@
 module IF_ID (
     input  wire        clk,
     input  wire        reset,
+    input wire         write_enable,
     input  wire [31:0] instr_in,
     input  wire [31:0] pc_in,
     input  wire [31:0] pcplus4_in,
@@ -16,7 +17,7 @@ module IF_ID (
             pc_out        <= 32'b0;
             pcplus4_out   <= 32'b0;
         end
-        else begin
+        else if (write_enable) begin
             instr_out     <= instr_in;
             pc_out        <= pc_in;
             pcplus4_out   <= pcplus4_in;
