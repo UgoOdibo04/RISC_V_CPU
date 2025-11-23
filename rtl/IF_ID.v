7577a0cb731bc9077291de8d9e89b8cce95d57dc
UgoOdibo04 <uodibo@charlotte.edu>
2025-11-22 22:24:42 -0500

    Update and rename if_id.v to IF_ID.v

---
+++ rtl/IF_ID.v
@@ -0,0 +1,27 @@
+module IF_ID (
+    input  wire        clk,
+    input  wire        reset,
+    input  wire [31:0] instr_in,
+    input  wire [31:0] pc_in,
+    input  wire [31:0] pcplus4_in,
+
+    output reg [31:0] instr_out,
+    output reg [31:0] pc_out,
+    output reg [31:0] pcplus4_out
+);
+
+    always @(posedge clk or posedge reset) begin
+        if (reset) begin
+            instr_out     <= 32'b0;
+            pc_out        <= 32'b0;
+            pcplus4_out   <= 32'b0;
+        end
+        else begin
+            instr_out     <= instr_in;
+            pc_out        <= pc_in;
+            pcplus4_out   <= pcplus4_in;
+        end
+    end
+
+endmodule
+
