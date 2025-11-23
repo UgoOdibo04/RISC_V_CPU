24d60b4bb0b375526e2d0b9719846b9176a2ded4
UgoOdibo04 <uodibo@charlotte.edu>
2025-11-22 22:59:21 -0500

    Update pc.v

--- rtl/pc.v
+++ rtl/pc.v
@@ -1,16 +1,15 @@
-module pc (
-    input        clock,
-    input         reset,
-    input   [31:0] next_pc,
+module pc(
+    input  wire        clock,
+    input  wire        reset,
+    input  wire        pc_write,   // NEW
+    input  wire [31:0] next_pc,
     output reg  [31:0] pc
 );
-
-  always @(posedge clock or posedge reset) begin
+    always @(posedge clock or posedge reset) begin
         if (reset)
-            pc <= 32'h00000000;   
-        else
+            pc <= 32'h00000000;
+        else if (pc_write)
             pc <= next_pc;
+        // else keep old pc (stall)
     end
-
 endmodule
-
